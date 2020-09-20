53202100a8c3a6ed7479c90a86a4efe41c6d8f0f
Rafeal <rafa.assugeni@gmail.com>
2020-09-20 21:27:08 +0530

    make product detail page dynamic

--- shopping/shopping_app/templatetags/custom_filters.py
+++ shopping/shopping_app/templatetags/custom_filters.py
@@ -19,3 +19,13 @@ def encode_space(title):
 def get_categories(title):
     path = os.path.join(BASE_DIR, 'media')
     return os.listdir(path=path)
+
+@register.filter
+def get_image_list(category,title):
+    path = os.path.join(BASE_DIR, 'media', category, title)
+    list_images = os.listdir(path=path)
+    return list_images
+
+@register.filter
+def get_rand(num):
+    return random.randint(10,num)
